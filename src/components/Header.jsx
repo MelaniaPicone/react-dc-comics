be3dc142fe31315abdd0c283bdda2459326800b4
MelaniaPicone <melania_picone@yahoo.it>
2025-09-02 07:20:03 +0200

    [feat] header menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,34 @@
 function Header() {
   return (
-    <h1>Header DC Comics</h1>
+<header>
+<div className="container">
+  <div className="row">
+
+    <div className="col-50">
+      <div id="logo-header">
+        <img src="./assets/img/dc-logo.png" />
+      </div>
+    </div>
+
+    <div className="col-50">
+      <div id="menu">
+        <li><a href="#">Characters</a></li>
+        <li><a href="#">Comics</a></li>
+        <li><a href="#">Movies</a></li>
+        <li><a href="#">Tv</a></li>
+        <li><a href="#">Games</a></li>
+        <li><a href="#">Collectibles</a></li>
+        <li><a href="#">Videos</a></li>
+        <li><a href="#">Fans</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Shop</a></li>
+      </div>
+    </div>
+  </div>
+</div>
+
+</header>
+
   );
 }
 
