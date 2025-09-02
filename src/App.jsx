9352d8a11734aef9f66a5c84f8d83efc61ec4139
MelaniaPicone <melania_picone@yahoo.it>
2025-09-02 06:42:21 +0200

    [fix] components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
- return ;
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
+
+
